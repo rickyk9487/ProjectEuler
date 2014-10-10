5ce73d37483ce1dbdd7b015e0169215007c876c2
rickyk9487 <rickyk9487@gmail.com>
2014-10-10 16:16:45 -0700

    updated answer
    previous solution did not return prime

--- Euler3.py
+++ Euler3.py
@@ -2,16 +2,18 @@
 Problem 3
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
-Answer: 486847
+Answer: 6857
 """
-import numpy as np
-
-n, i = 600851475143, 3
-# The given integer to factor is not even
-prime_factors = []
-while i <= np.sqrt(n)+1:
+n, i, prime_factors = 600851475143, 3, []
+# know n is not even, so start at 3 to see if it's a factor
+while n != 1:
     if n % i == 0:
         prime_factors.append(i)
-    i += 2
-    
+        # keep track of all prime factors
+        n = n/i
+    else:
+        # increment factors through all odd numbers
+        i += 2
+        
 print prime_factors[-1] 
+# only print the last prime factor
