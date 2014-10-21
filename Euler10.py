64198568a2e9e497acc296c6cf3c366f044cfcfb
rickyk9487 <rickyk9487@gmail.com>
2014-10-20 18:41:20 -0700

    Project Euler 10, sum of primes
    Use Sieve of Eratosthenes to find the sum of all primes below two
    million.

---
+++ Euler10.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Summation of primes
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below (strictly) two million.
+
+Answer: 142913828922
+Ricky Kwok, rickyk9487@gmail.com, 2014-10-20
+"""
+
+def prime_sum(n):
+    if n < 2: return 0
+    if n == 2: return 2
+    if n % 2 == 0: n += 1
+    primes = [True] * n
+    primes[0:2] = [None] * 2
+    sum = 0
+    for ind,val in enumerate(primes):
+        if val is True and ind > n ** 0.5 + 1:
+            sum += ind
+        elif val is True:
+            primes[ind*2::ind] = [False] * (((n - 1)//ind) - 1)
+            sum += ind
+    return sum
+ 
+sum = prime_sum(2*10**6)
+
+print sum
